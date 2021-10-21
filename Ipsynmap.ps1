16d0adb68aedfde41a46d9b73fee422ff56dda59
Gerardo0202 <88344050+Gerardo0202@users.noreply.github.com>
2021-10-20 22:19:00 -0500

    IPs y nmaps

---
+++ Ipsynmap.ps1
@@ -0,0 +1,27 @@
+﻿
+function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+Write-Host("Mi IP es $IP")
+
+function GetIPp{
+(curl 'https://ifconfig.me/ip') 
+}
+$IPP = GetIPp
+Write-Host ("Mi IP publica es $IPP")
+
+function Getnmap{
+(nmap 192.168.1.103)
+}
+$scan = Getnmap
+Write-Host ("$scan")
+
+function GetPnmap{
+(nmap google.com)
+}
+$Pscan = GetPnmap
+Write-Host ("$Pscan")
+
+
